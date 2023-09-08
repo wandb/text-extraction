f6140c7a47197545fa8a640f4f2abd9b27ce6207
Shawn Lewis <shlewis@gmail.com>
2023-09-08 10:04:39 -0700

    Update util

--- util.py
+++ util.py
@@ -1,4 +1,17 @@
+import typing
 import wandb
+import weave
+
+# HACK, launch doesn't give us /tmp space
+import os
+os.environ['WEAVE_LOCAL_ARTIFACT_DIR'] = './weave-fs'
+
+# HACK, Weave Object type can't be deserialized since it doesn't
+# exist in pypi package yet. We need to handle anonymous objects in Weave
+@weave.type()
+class Dataset:
+    rows: list[typing.Any]
+
 
 def make_wandb_table(list_of_dicts):
     table = wandb.Table(columns=list(list_of_dicts[0].keys()), allow_mixed_types=True)
