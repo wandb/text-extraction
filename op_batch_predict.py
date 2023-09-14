fe204faa2de725b50a05932db33d299366d55caa
Shawn Lewis <shlewis@gmail.com>
2023-09-14 14:22:28 -0700

    prints in batch predict

--- op_batch_predict.py
+++ op_batch_predict.py
@@ -7,6 +7,8 @@ import base_types
 @weave.op(input_type={"model": weave.types.ObjectType()})
 def batch_predict(dataset: base_types.Dataset, model) -> typing.Any:
     predict_result = []
+    print("DATASET", dataset)
+    print("MODEL", model)
 
     for dataset_row in dataset.rows:
         result = weave.use(model.predict(dataset_row["example"]))
