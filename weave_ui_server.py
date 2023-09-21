0947e0b2055592dd20e95439a46465e568496a84
Shawn Lewis <shlewis@gmail.com>
2023-09-21 06:13:29 -0700

    fix weave_ui_server

--- weave_ui_server.py
+++ weave_ui_server.py
@@ -1,5 +1,28 @@
-from weave import server
+import time
 
-s = server.HttpServer()
-print(s.url)
-s.run()
+from weave import context
+from weave import util
+
+
+def force_is_notebook():
+    return True
+
+
+util.is_notebook = force_is_notebook
+
+# server starts here
+context.get_client()
+
+
+def remove_suffix(input_str, suffix):
+    if input_str.endswith(suffix):
+        return input_str[: -len(suffix)]
+    return input_str
+
+
+frontend_url = context.get_frontend_url()
+
+print(remove_suffix(frontend_url, "/__frontend/weave_jupyter"))
+
+while True:
+    time.sleep(1)
